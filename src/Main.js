bef1a7fa55bf3e372d880f2152ed265842f6ccec
Luciano <luchosr@gmail.com>
2019-05-20 09:17:26 -0300

    add Main component

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { FaDivide } from "react-icons/fa";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Main>
+          <JSreddit />
+        </Main>
+      </div>
+    );
+  }
+}
+
+export default Main;
